db2627e90b2f8f77da7d2c1bb67e510c1808fcae
nika <nika.alinejad@gmail.com>
2025-12-18 21:05:55 -0700

    Remove unnecessary comments in booking.js
    Refactored comments to improve clarity and organization.

--- scripts/booking.js
+++ scripts/booking.js
@@ -1,7 +1,4 @@
-/********* create variables *********/
-// useful variables might be: the cost per day, the number of days selected, and elements on the screen that will be clicked or will need to be modified. 
-// Do any of these variables need to be initialized when the page is loaded? 
-// When do they need to be reset or updated?
+//create variables
 
 let costPerDay = 35;
 let dayCounter = 0; //number of days selected (counter)
@@ -18,12 +15,11 @@ const fullButton = document.getElementById("full");
 const halfButton = document.getElementById("half");
 const clearButton = document.getElementById("clear-button");
 const calculatedCost = document.getElementById("calculated-cost");
-
 //for the rest I used const identifiers because they should not be re-assigned or updated.
 
-/********* colour change days of week *********/
-// when the day buttons are clicked, we will apply the "clicked" class to that element, and update any other relevant variables. Then, we can recalculate the total cost.
-// added challenge: don't update the dayCounter if the same day is clicked more than once. hint: .classList.contains() might be helpful here!
+//color changes logic
+//when the day buttons are clicked, "clicked" class will be applied to that element, and update any other relevant variables.
+//doesn't update dayCounter if the same day is clicked more than once.
 
 function whenClicked(dayButton) {
     if (!dayButton.classList.contains("clicked")) {
@@ -39,8 +35,8 @@ wednesdayButton.onclick= function() {whenClicked(wednesdayButton);};
 thursdayButton.onclick = function() {whenClicked(thursdayButton);};
 fridayButton.onclick = function() {whenClicked(fridayButton);};
 
-/********* clear days *********/
-// when the clear-button is clicked, the "clicked" class is removed from all days, any other relevant variables are reset, and the calculated cost is set to 0.
+//clear days logic
+//"clicked" class removed from all days when clear button is clicked
 
 clearButton.onclick = function() {
     mondayButton.classList.remove("clicked");
@@ -54,8 +50,8 @@ clearButton.onclick = function() {
     calculate();
 };
 
-/********* change rate *********/
-// when the half-day button is clicked, set the daily rate to $20, add the "clicked" class to the "half" element, remove it from the "full" element, and recalculate the total cost.
+//change rate logic
+//when the half-day button clicked: daily rate = $20
 
 halfButton.onclick = function() { 
     costPerDay = 20;
@@ -64,7 +60,7 @@ halfButton.onclick = function() {
     calculate();
 };
 
-// when the full-day button is clicked, the daily rate is set back to $35, the clicked class is added to "full" and removed from "half", and the total cost is recalculated.
+//when the half-day button clicked: daily rate = $35
 
 fullButton.onclick = function() {
     costPerDay = 35;
@@ -73,8 +69,7 @@ fullButton.onclick = function() {
     calculate();
 };
 
-/********* calculate *********/
-// when a calculation is needed, set the innerHTML of the calculated-cost element to the appropriate value
+//processing & calculation
 
 function calculate() {
     totalCost = dayCounter * costPerDay;
